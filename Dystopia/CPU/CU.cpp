0efdd7e83ef1c8d5e9285478de79f744f5fd93f5
nogitsune-youkai <nikita.elin2011@yandex.ru>
2024-07-20 19:40:00 +0300

    Some refactoring

--- Dystopia/CPU/CU.cpp
+++ Dystopia/CPU/CU.cpp
@@ -23,13 +23,16 @@ void CU::Fetch()
 
 void CU::Execute()
 {
-
+	// the code in this function is only for test purposes for now and will be rewritten
 	Instruction inst;
 	switch (opcodes) {
 	case Opcodes::CMC:
 		//std::cout << this->general_purpose_registers.EFLAGS.test(CF);
 		inst.CMC();
+		break;
 		//std::cout << this->general_purpose_registers.EFLAGS.test(CF);
+	default:
+		std::cout << "Unknown opcode";
 	}
 }
 
