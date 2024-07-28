07d25ad02fe11cfa9fd5516620edda8ab6765f3e
nogitsune-youkai <nikita.elin2011@yandex.ru>
2024-07-28 07:59:25 +0300

    More PE format definitions

--- Dystopia/Core/Loader/PE.h
+++ Dystopia/Core/Loader/PE.h
@@ -33,15 +33,70 @@ enum Characteristics {
 };
 
 struct PE32OptionalHeader {
+	// Standard fields
 	WORD Magic; // state of the image file. The most common number is 0x10B, which identifies it as a normal executable file. 0x107 - ROM image, and 0x20B - PE32+ executable. 
 	BYTE MajorLinkerVersion;
 	BYTE MinorLinkerVersion;
 	DWORD SizeOfCode; // The size of the code (text) section, or the sum of all code sections if there are multiple sections. 
+	DWORD SizeOfInitializedData;
 	DWORD SizeOfUninitializedData; // The size of the uninitialized data section(BSS), or the sum of all such sections if there are multiple BSS sections.
 	DWORD AddressOfEntryPoint; // The address of the entry point relative to the image base when the executable file is loaded into memory. If no entry point is present, this field must be 0.                         
 	DWORD BaseOfCode; // The address that is relative to the image base of the beginning - of - code section when it is loaded into memory.
-	DWORD BaseOfData; // The address that is relative to the image base of the beginning-of-data section when it is loaded into memory. This field is absent to PE32+ format
-	QWORD ImageBase; // The preferred address of the first byte of image when loaded into memory; must be a multiple of 64 K.The default for Windows NT is 0x00400000. 
+	//DWORD BaseOfData; // The address that is relative to the image base of the beginning-of-data section when it is loaded into memory. This field is absent to PE32+ format
+	
+	// Windows-Specific Fields
+	QWORD ImageBase; // The preferred address of the first byte of image when loaded into memory; must be a multiple of 64 K.The default for Windows NT is 0x00400000. Might be a good idea to define it as bit-field
+	DWORD SectionAlignment;
+	DWORD FileAlignment;
+	WORD MajorOperatingSystemVersion;
+	WORD MinorOperatingSystemVersion;
+	WORD MajorImageVersion;
+	WORD MinorImageVersion;
+	WORD MajorSubsystemVersion;
+	WORD MinorSubsystemVersion;
+	DWORD Win32VersionValue; // Reserved
+	DWORD SizeOfImage;
+	DWORD SizeOfHeaders;
+	DWORD CheckSum;
+	WORD Subsystem;
+	WORD DllCharacteristics; 
+	QWORD SizeOfStackReserve;
+	QWORD SizeOfStackCommit;
+	QWORD SizeOfHeapReserve;
+	QWORD SizeOfHeapCommit;
+	DWORD LoaderFlags; // Reserved
+	DWORD NumberOfRvaAndSizes;
+};
+
+enum WindowsSubsystem {
+	IMAGE_SUBSYSTEM_UNKNOWN, // An unknown subsystem 
+	IMAGE_SUBSYSTEM_NATIVE, // Device drivers and native Windows processes 
+	IMAGE_SUBSYSTEM_WINDOWS_GUI, // The Windows graphical user interface (GUI) subsystem 
+	IMAGE_SUBSYSTEM_WINDOWS_CUI, // The Windows character subsystem 
+	IMAGE_SUBSYSTEM_OS2_CUI = 5, // The OS / 2 character subsystem
+	IMAGE_SUBSYSTEM_POSIX_CUI = 7, // The Posix character subsystem 
+	IMAGE_SUBSYSTEM_NATIVE_WINDOWS, // Native Win9x driver 
+	IMAGE_SUBSYSTEM_WINDOWS_CE_GUI, // Windows CE 
+	IMAGE_SUBSYSTEM_EFI_APPLICATION, // An Extensible Firmware Interface (EFI) application 
+	IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER, // An EFI driver with boot services
+	IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER, // An EFI driver with run-time services
+	IMAGE_SUBSYSTEM_EFI_ROM, //	An EFI ROM image
+	IMAGE_SUBSYSTEM_XBOX, // XBOX
+	IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION = 16 // Windows boot application. 
+};
+
+enum DllCharacteristics {
+	IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA = 0x0020, // Image can handle a high entropy 64-bit virtual address space. 
+	IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE = 0x0040, // DLL can be relocated at load time. 
+	IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY = 0x0080, // Code Integrity checks are enforced.
+	IMAGE_DLLCHARACTERISTICS_NX_COMPAT = 0x0100, // Image is NX compatible. 
+	IMAGE_DLLCHARACTERISTICS_NO_ISOLATION = 0x0200, // Isolation aware, but do not isolate the image. 
+	IMAGE_DLLCHARACTERISTICS_NO_SEH = 0x0400, // Does not use structured exception (SE) handling. No SE handler may be called in this image. 
+	IMAGE_DLLCHARACTERISTICS_NO_BIND = 0x0800, // Do not bind the image. 
+	IMAGE_DLLCHARACTERISTICS_APPCONTAINER = 0x1000, // Image must execute in an AppContainer. 
+	IMAGE_DLLCHARACTERISTICS_WDM_DRIVER = 0x2000, // A WDM driver. 
+	IMAGE_DLLCHARACTERISTICS_GUARD_CF = 0x4000, // Image supports Control Flow Guard. 
+	IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE = 0x8000, // Terminal Server aware. 
 };
 
 // this class describes Portable executable format
