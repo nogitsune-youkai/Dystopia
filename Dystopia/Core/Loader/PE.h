dd36db6066622b77d23258a769f4a8f67cbab997
nogitsune-youkai <nikita.elin2011@yandex.ru>
2024-07-29 08:33:43 +0300

    More definitions for PE format

--- Dystopia/Core/Loader/PE.h
+++ Dystopia/Core/Loader/PE.h
@@ -13,6 +13,39 @@ struct PEHeader {
 	std::bitset<16> Characteristics; // The flags that indicate the attributes of the file
 };
 
+enum MachineTypes {
+	IMAGE_FILE_MACHINE_UNKNOWN = 0x0,
+	IMAGE_FILE_MACHINE_ALPHA = 0x184,
+	IMAGE_FILE_MACHINE_ALPHA64 = 0x284,
+	IMAGE_FILE_MACHINE_AM33 = 0x1d3,
+	IMAGE_FILE_MACHINE_AMD64 = 0x8664,
+	IMAGE_FILE_MACHINE_ARM = 0x1c0,
+	IMAGE_FILE_MACHINE_ARM64 = 0xaa64,
+	IMAGE_FILE_MACHINE_ARMNT = 0x1c4,
+	IMAGE_FILE_MACHINE_AXP64 = 0x284,
+	IMAGE_FILE_MACHINE_EBC = 0xebc,
+	IMAGE_FILE_MACHINE_I386 = 0x14c,
+	IMAGE_FILE_MACHINE_IA64 = 0x200,
+	IMAGE_FILE_MACHINE_LOONGARCH32 = 0x6232,
+	IMAGE_FILE_MACHINE_LOONGARCH64 = 0x6264,
+	IMAGE_FILE_MACHINE_M32R = 0x9041,
+	IMAGE_FILE_MACHINE_MIPS16 = 0x266,
+	IMAGE_FILE_MACHINE_MIPSFPU = 0x366,
+	IMAGE_FILE_MACHINE_MIPSFPU16 = 0x466,
+	IMAGE_FILE_MACHINE_POWERPC = 0x1f0,
+	IMAGE_FILE_MACHINE_POWERPCFP = 0x1f1,
+	IMAGE_FILE_MACHINE_R4000 = 0x166,
+	IMAGE_FILE_MACHINE_RISCV32 = 0x5032,
+	IMAGE_FILE_MACHINE_RISCV64 = 0x5064,
+	IMAGE_FILE_MACHINE_RISCV128 = 0x5128,
+	IMAGE_FILE_MACHINE_SH3 = 0x1a2,
+	IMAGE_FILE_MACHINE_SH3DSP = 0x1a3,
+	IMAGE_FILE_MACHINE_SH4 = 0x1a6,
+	IMAGE_FILE_MACHINE_SH5 = 0x1a8,
+	IMAGE_FILE_MACHINE_THUMB = 0x1c2,
+	IMAGE_FILE_MACHINE_WCEMIPSV2 = 0x169
+};
+
 enum Characteristics {
 	IMAGE_FILE_RELOCS_STRIPPED = 0x0001,
 	IMAGE_FILE_EXECUTABLE_IMAGE = 0x0002,
@@ -54,7 +87,7 @@ struct PE32OptionalHeader {
 	WORD MinorImageVersion;
 	WORD MajorSubsystemVersion;
 	WORD MinorSubsystemVersion;
-	DWORD Win32VersionValue; // Reserved
+	DWORD Win32VersionValue; // Reserved, must be 0 
 	DWORD SizeOfImage;
 	DWORD SizeOfHeaders;
 	DWORD CheckSum;
@@ -65,7 +98,23 @@ struct PE32OptionalHeader {
 	QWORD SizeOfHeapReserve;
 	QWORD SizeOfHeapCommit;
 	DWORD LoaderFlags; // Reserved
-	DWORD NumberOfRvaAndSizes;
+	DWORD NumberOfRvaAndSizes; // The number of data-directory entries in the remainder of the optional header. Each describes a location and size. 
+	QWORD ExportTable; // The export table address and size. For more information see .edata Section (Image Only). 
+	QWORD ImportTable; // The import table address and size. For more information, see The .idata Section.
+	QWORD ResourceTable; // The resource table address and size. For more information, see The .rsrc Section.
+	QWORD ExceptionTable; // The exception table address and size. For more information, see The .pdata Section. 
+	QWORD CertificateTable; // The attribute certificate table address and size. For more information, see The Attribute Certificate Table (Image Only). 
+	QWORD BaseRelocationTable; // The base relocation table address and size. For more information, see The .reloc Section (Image Only).
+	QWORD Debug; // The debug data starting address and size. For more information, see The .debug Section.
+	QWORD Architecture; // Reserved, must be 0 
+	QWORD GlobalPtr; // The RVA of the value to be stored in the global pointer register. The size member of this structure must be set to zero. 
+	QWORD TLSTable; // The thread local storage (TLS) table address and size. For more information, see The .tls Section.
+	QWORD LoadConfigTable; // The load configuration table address and size. For more information, see The Load Configuration Structure (Image Only).
+	QWORD BoundImport; // The bound import table address and size.
+	QWORD IAT; // The import address table address and size. For more information, see Import Address Table.
+	QWORD DelayImportDescriptor; // The delay import descriptor address and size. For more information, see Delay-Load Import Tables (Image Only).
+	QWORD CLRRuntimeHeader; // The CLR runtime header address and size. For more information, see The .cormeta Section (Object Only).
+	QWORD ReservedField;
 };
 
 enum WindowsSubsystem {
@@ -98,6 +147,11 @@ enum DllCharacteristics {
 	IMAGE_DLLCHARACTERISTICS_GUARD_CF = 0x4000, // Image supports Control Flow Guard. 
 	IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE = 0x8000, // Terminal Server aware. 
 };
+
+struct _IMAGE_DATA_DIRECTORY {
+	DWORD VirtualAddress; // RVA of the table
+	DWORD Size; // size in bytes
+} IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY;
 
 // this class describes Portable executable format
 class PE
