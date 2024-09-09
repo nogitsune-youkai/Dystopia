f706075b500fa915d2496ddf82f240177fa4a7dc
nogitsune-youkai <nikita.elin2011@yandex.ru>
2024-09-09 21:34:20 +0300

    Added constants for MIPS processors

--- Dystopia/Core/Loader/PE.h
+++ Dystopia/Core/Loader/PE.h
@@ -453,6 +453,24 @@ constexpr UCHAR IMAGE_REL_IA64_GPREL32 = 0x001c; // A 32 - bit GP - relative fix
 constexpr UCHAR IMAGE_REL_IA64_ADDEND = 0x001F; // The relocation is valid only when it immediately follows one of the following relocations : IMM14, IMM22, IMM64, GPREL22, LTOFF22, LTOFF64, SECREL22, SECREL64I, or SECREL32.Its value contains the addend to apply to instructions within a bundle, not for data.
 
 
+// MIPS Processors. The following relocation type indicators are defined for MIPS processors.
+constexpr UCHAR IMAGE_REL_MIPS_ABSOLUTE = 0x0000; // The relocation is ignored.
+constexpr UCHAR IMAGE_REL_MIPS_REFHALF = 0x0001; // The high 16 bits of the target's 32-bit VA. 
+constexpr UCHAR IMAGE_REL_MIPS_REFWORD = 0x0002; // The target's 32-bit VA. 
+constexpr UCHAR IMAGE_REL_MIPS_JMPADDR = 0x0003; // The low 26 bits of the target's VA. This supports the MIPS J and JAL instructions. 
+constexpr UCHAR IMAGE_REL_MIPS_REFHI = 0x0004; // The high 16 bits of the target's 32-bit VA. This is used for the first instruction in a two-instruction sequence that loads a full address.
+constexpr UCHAR IMAGE_REL_MIPS_REFLO = 0x0005; // The low 16 bits of the target's VA. 
+constexpr UCHAR IMAGE_REL_MIPS_GPREL = 0x0006; // A 16 - bit signed displacement of the target relative to the GP register.
+constexpr UCHAR IMAGE_REL_MIPS_LITERAL = 0x0007; // The same as IMAGE_REL_MIPS_GPREL.
+constexpr UCHAR IMAGE_REL_MIPS_SECTION = 0x000A; // The 16 - bit section index of the section contains the target.This is used to support debugging information.
+constexpr UCHAR IMAGE_REL_MIPS_SECREL = 0x000B; // The 32 - bit offset of the target from the beginning of its section.This is used to support debugging information and static thread local storage.
+constexpr UCHAR IMAGE_REL_MIPS_SECRELLO = 0x000C; // The low 16 bits of the 32 - bit offset of the target from the beginning of its section.
+constexpr UCHAR IMAGE_REL_MIPS_SECRELHI = 0x000D; // The high 16 bits of the 32 - bit offset of the target from the beginning of its section.An IMAGE_REL_MIPS_PAIR relocation must immediately follow this one.
+constexpr UCHAR IMAGE_REL_MIPS_JMPADDR16 = 0x0010; // The low 26 bits of the target's VA. This supports the MIPS16 JAL instruction. 
+constexpr UCHAR IMAGE_REL_MIPS_REFWORDNB = 0x0022; // The target's 32-bit RVA. 
+constexpr UCHAR IMAGE_REL_MIPS_PAIR = 0x0025; // The relocation is valid only when it immediately follows a REFHI or SECRELHI relocation.Its SymbolTableIndex contains a displacement and not an index into the symbol table.
+
+
 // this class describes Portable executable format
 class PE
 {
